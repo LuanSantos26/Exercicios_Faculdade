47e2e2e74e5afd64b618bebe6bef66b09a97b750
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2025-05-13 19:52:27 -0300

    Create laco_repticao.py

---
+++ laco_repticao.py
@@ -0,0 +1,5 @@
+numero=int(input("Digite um número inteiro: "))
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+# O programa solicita ao usuário um número inteiro e, em seguida, utiliza um loop para calcular e exibir a tabuada desse número de 1 a 10.
