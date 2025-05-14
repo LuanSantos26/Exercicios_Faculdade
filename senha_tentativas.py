e1bc17a2d86a31a5a38e19adc5e779f2281135dd
Luan Feitosa Santos <luansantos26@outlook.com>
2025-05-13 22:32:31 -0300

    resolução da senha 13/05/2025

--- senha_tentativas.py
+++ senha_tentativas.py
@@ -1,17 +1,10 @@
-senha = input("Digite a sua senha de acesso : ").lower
-confirmacao = input("Confirme sua senha novamente :").lower
-tentativas = 0
-limite = 3
-
-while limite < tentativas:
-    senha = input("Digite a sua senha de acesso : ").lower()
-    confirmacao = input("Confirme sua senha novamente :").lower()
-    if senha == confirmacao:
-        print("Senha correta, Acesso permitido !")
+senha = input("Digite a senha: ")
+tentativas = 0  
+while tentativas < 3:
+    senha_digitada = input("Digite a senha novamente: ")
+    if senha_digitada == senha:
+        print("Senha correta!")
         break
-    elif senha != confirmacao:
-        print("Senha incorreta, Acesso negado !")
-        limite -= 1
-        if tentativas == limite:
-            print("Limite de tentativas atingido, Acesso negado !")
-  
+    else:
+        tentativas += 1
+        print(f"Senha incorreta! Você tem {3 - tentativas} tentativas restantes.")
