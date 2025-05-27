6345b50c31e5aa3263ee2dba8b3b9c87da7611cb
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2025-05-27 20:38:26 -0300

    exercicio de lista

---
+++ exercicio_lista.py
@@ -0,0 +1,12 @@
+
+#Crie uma lista que receba elemento por elemento do teclado por usuario e coloque em uma lista.
+def lista_feira():
+    add_lista = input("Digite algo que queira adiconar a sua lista: ")
+    lista=[]
+    lista.append (add_lista)
+    print (f"você tem esses itens {lista}")
+    
+lista_feira()
+
+
+
