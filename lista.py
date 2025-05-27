02273c6e06b77650a3996b33f289c39c95691ab6
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2025-05-27 20:13:18 -0300

    .

--- lista.py
+++ lista.py
@@ -1,18 +1,17 @@
 lista=['maça, banana, laranja, uva, abacaxi, morango, kiwi, pêra, melancia, manga']
-def lista_frutas():
-    print('Lista de frutas:')
-    for fruta in lista[0].split(', '):
-        print(fruta.strip())
-def adicionar_fruta(fruta):
-    if fruta not in lista[0]:
-        lista[0] += ', ' + fruta
-        print(f'{fruta} foi adicionada à lista.')
-    else:
-        print(f'{fruta} já está na lista.')
-def remover_fruta(fruta):
-    if fruta in lista[0]:
-        lista[0] = ', '.join([f for f in lista[0].split(', ') if f.strip() != fruta])
-        print(f'{fruta} foi removida da lista.')
-    else:
-        print(f'{fruta} não está na lista.')
+print('Lista de frutas:', lista)
+for fruta in lista[0].split(', '):
+    print(fruta.strip())
+#def adicionar_fruta(fruta):
+ #   if fruta not in lista[0]:
+  #      lista[0] += ', ' + fruta
+    #    print(f'{fruta} foi adicionada à lista.')
+    #else:
+     #   print(f'{fruta} já está na lista.')
+#def remover_fruta(fruta):
+ #   if fruta in lista[0]:
+  #      lista[0] = ', '.join([f for f in lista[0].split(', ') if f.strip() != fruta])
+   #     print(f'{fruta} foi removida da lista.')
+ #   else:
+#        print(f'{fruta} não está na lista.')
         
