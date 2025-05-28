cf8a55a89adf6ef7b619eb4daadbf09890199266
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2025-05-27 21:06:21 -0300

    .

--- calcular_media_lista.py
+++ calcular_media_lista.py
@@ -1,1 +1,5 @@
 #calcule a media aritimetica dos elementos da lista a seguir:notas=[7,8,6,6,10]
+notas = [7, 8, 6, 6, 10]
+soma = sum(notas)
+media = soma / len(notas)
+print("A média aritmética das notas é:", media)
--- exercicio_lista.py
+++ exercicio_lista.py
@@ -1,6 +1,10 @@
 
 #Crie uma lista que receba elemento por elemento do teclado por usuario e coloque em uma lista.
-item=input("digite o item que queira adiconar em sua lista: ")
-lista=[].append(item)
-for item in lista:
-    novo_item=input("digite um novo item em sua lista: ", [item+1])
+lista = []
+for i in range(5):  # Permite ao usuário adicionar 5 elementos
+    elemento = input("Digite um elemento para adicionar à lista  o permitido no momento é 5 (ou 'sair' para encerrar): ")
+    if elemento.lower() == 'sair':
+        break
+    lista.append(elemento)
+
+print("Lista completa:", lista) 
